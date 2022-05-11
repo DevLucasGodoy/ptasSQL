d567f9873b38dd70d4ff3639c3a089e8874cdded
Lucas Godoy <54687280+DevLucasGodoy@users.noreply.github.com>
2022-05-11 20:52:54 +0000

    Update Routes for empresas

--- index.js
+++ index.js
@@ -7,28 +7,54 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-app.get("/", async function(req, res){
-var mostrar = await usuario.findAll();
-  res.json(mostrar);
-});
-
-app.get("/:id", async function(req, res) {
- const id = await usuario.findByPk(req.params.id);
- res.json(id)
-});
-
-app.post("/", async function(req, res){
+app.post("/usuarios", async function(req, res){
   var mostrar = await usuario.create(req.body);
   res.json(mostrar);
 });
 
-app.put("/:id", async function(req, res){
+app.get("/usuarios", async function(req, res){
+var mostrar = await usuario.findAll();
+  res.json(mostrar);
+});
+
+app.get("/usuarios/:id", async function(req, res) {
+ const id = await usuario.findByPk(req.params.id);
+ res.json(id)
+});
+
+app.put("/usuarios/:id", async function(req, res){
   var resultado = usuario.update(req.body,{ where: { id: req.params.id }});
   res.json(resultado);
 });
 
-app.delete("/", async function(req, res){
+app.delete("/usuarios", async function(req, res){
   usuario.destroy({ where: { id: 1 }});
+});
+
+/////////////////////////////////////////////////////////////////////////////
+
+app.post("/empresas", async function(req, res){
+  var mostrar = await usuario.create(req.body);
+  res.json(mostrar);
+});
+
+app.get("/empresas", async function(req, res){
+var mostrar = await empresa.findAll();
+  res.json(mostrar);
+});
+
+app.get("/empresas/:id", async function(req, res) {
+ const id = await empresa.findByPk(req.params.id);
+ res.json(id)
+});
+
+app.put("/:id", async function(req, res){
+  var resultado = empresa.update(req.body,{ where: { id: req.params.id }});
+  res.json(resultado);
+});
+
+app.delete("/empresas", async function(req, res){
+  empresa.destroy({ where: { id: 1 }});
 });
 
 app.listen(3000, function(){
